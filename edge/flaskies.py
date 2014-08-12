3dc2276e2e868bffc48851404ef2f29199f77732
Matt Rasband <matt.rasband@edgeresearchlab.com>
2014-08-11 22:39:53 -0400

    Comment updates, removed redundant call

--- edge/flaskies.py
+++ edge/flaskies.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
 
+from __future__ import print_function
 from flask import request, Response
 import functools
 import json
@@ -23,26 +25,34 @@ def endpoint(consumes=None, produces='text/plain'):
             if produces == media_type_json:
                 message = {}
 
+            # Verify the header matches the type we want to consume.
+            # TODO - this should support regex since some sites (such as github)
+            # use things like 'application/json+addeddata'
             if consumes:
-                if consumes:
-                    if not 'Content-Type' in request.headers \
-                        or request.headers['Content-Type'] != consumes:
-                        return Response(json.dumps({"message" : "Unaccepted content type."}), status=400)
+                if not 'Content-Type' in request.headers \
+                    or request.headers['Content-Type'] != consumes:
+                    return Response(json.dumps({"message" : "Unaccepted content type."}), status=400)
 
             try:
                 result_code, message = func(*args, **kwargs)
                 if result_code is None:
                     result_code = 200 # assume things are good
             except WebException as e:
+                # These are likely only be client type exceptions, 4xx
+                # raised from any decorated method.
                 result_code = e.status_code
                 message = e.message
             except Exception as e:
+                # Any other exception type is treated as our issue as the server.
                 result_code = 500
                 message = e.message
 
+            # If json is used, encode it for the user so they can just return a dict
+            # and not worry about it.
             if produces == media_type_json:
                 message = json.JSONEncoder().encode(message)
 
             return Response(message, status=result_code, mimetype=produces)
+
         return wrapper
     return endpoint_decor
