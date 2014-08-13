224f434b97b381c9d31b97d0e41032213581c721
Matt Rasband <matt.rasband@edgeresearchlab.com>
2014-08-12 22:05:38 -0400

    added access to the vors collection

--- edge/persistence.py
+++ edge/persistence.py
@@ -2,30 +2,54 @@ from pymongo import MongoClient
 
 class DBClient:
     def __init__(self, server, port, database, username, password):
-        uri = "mongodb://{user}:{password}@{server}:{port}/{database}".format(user=username, password=password, server=server, port=port, database=database)
+        login_format = {
+            'username': username,
+            'password': password,
+            'server': server,
+            'port': port,
+            'database': database
+        }
+        uri = 'mongodb://{username}:{password}@{server}:{port}/{database}'.format(**login_format)
         self.client = MongoClient(uri)
         self.db = self.client[database]
         self.flights_collection = self.db.flights
         self.tracking_collection = self.db.trackers
+        self.vor_collection = self.db.vors
 
     def latest_flight(self):
         '''Grab the most recent flight'''
-        return self.flights_collection.find_one(sort=[("start", -1)])
+        return self.flights_collection.find_one(sort=[('start', -1)])
 
     def this_flights_points(self):
         '''Grab all the points for the most recent flight.'''
         this_flight = self.latest_flight()
-        cur = self.tracking_collection.find({"transmit_time" : {"$gt" : this_flight['begin']}}).sort("transmit_time")
+        filters = {
+            'transmit_time': {
+                '$gt': this_flight['begin']
+            }
+        }
+        cur = self.tracking_collection.find(filters).sort('transmit_time')
+        return [x for x in cur]
+
+    def get_vor_points(self, filters=dict()):
+        cur = self.vor_collection.find(filters)
         return [x for x in cur]
 
     def specific_flights_points(self, flightname):
         pass
 
     def tracking_point_exists(self, identifier, transmit_time):
-        found = self.tracking_collection.find({"transmit_time" : transmit_time, "edge_id" : identifier})
+        filters = {
+            'transmit_time': transmit_time,
+            'edge_id': identifier
+        }
+        found = self.tracking_collection.find(filters)
         return True if found else False
 
     def save_tracking_point(self, tracking_point):
         '''Save a tracking point to the database.'''
-        # TODO - Enforce a minimum schema (even though that isn't required for mongo)
         return self.tracking_collection.insert(tracking_point)
+
+    def save_flight_info(self, flight_info):
+        '''Save the information on a flight into the flights collection'''
+        return self.flights_collection.insert(flight_info)
